4a9ec6fa3ac8926fa95dcd0b530af3d424d2d1fd
saikiran2001 <bjsaikiran@gmail.com>
2023-12-09 15:49:23 +0000

    Revert "parasite: sepolicy: Include turbo_adapter sepolicies"
    This reverts commit e533fea1f6ae50582576b211a3b2834ab8cf28a0.

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -40,7 +40,3 @@ endif
 # google_battery service
 BOARD_SEPOLICY_DIRS += \
     hardware/google/pixel-sepolicy/googlebattery
-
-# turbo_adapter
-BOARD_SEPOLICY_DIRS += \
-    hardware/google/pixel-sepolicy/turbo_adapter
